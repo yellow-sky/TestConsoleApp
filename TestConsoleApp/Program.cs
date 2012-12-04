aa5c986ca5ac66858d3844f59cb6b791574fed10
yellow-sky <nikulin.e@gmail.com>
2012-12-04 17:22:37 +0400

    Update TestConsoleApp/Program.cs

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -10,6 +10,7 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New integration level!")
             Console.Read();
         }
     }
